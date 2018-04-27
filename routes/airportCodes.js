2ab907a6e5d821723faef0d5b240ba70cade06fb
rimtranhs <rim.tran@hotschedules.com>
2018-04-27 15:22:57 +0700

    Init authentication V4

--- routes/airportCodes.js
+++ routes/airportCodes.js
@@ -1,87 +1,166 @@
 var express = require('express');
 var router = express.Router();
 var AWS = require('aws-sdk');
-var hmacSHA256 = require('crypto-js/hmac-sha256');
-var sha256 = require('crypto-js/sha256');
-var base64 = require('crypto-js/enc-base64');
-var hexEncode = require('crypto-js/enc-hex');
 var moment = require('moment');
 var fetch = require('node-fetch');
 
-var findIndex = require('lodash/findIndex');
+const stringToSign = (request) => {
+  var r = request;
 
-AWS.config.loadFromPath('./config/config.json');
-var s3 = new AWS.S3();
+  var parts = [];
+  parts.push(r.method);
+  parts.push(r.headers['Content-MD5'] || '');
+  parts.push(r.headers['Content-Type'] || '');
 
-var bucketParams = {
-  Bucket: "kms-hs-production-management",
-  Key: 'vendors.json',
+  // This is the "Date" header, but we use X-Amz-Date.
+  // The S3 signing mechanism requires us to pass an empty
+  // string for this Date header regardless.
+  parts.push(r.headers['presigned-expires'] || '');
+
+  var headers = canonicalizedAmzHeaders(request);
+  if (headers) parts.push(headers);
+  parts.push(canonicalizedResource(request));
+
+  return parts.join('\n');
+
 };
 
-// s3.createBucket({Bucket: bucketName}, function() {
-//   var params = {Bucket: bucketName, Key: keyName, Body: 'Hello World!'};
-//   s3.putObject(params, function(err, data) {
-//     if (err)
-//       console.log(err)
-//     else
-//       console.log("Successfully uploaded data to " + bucketName + "/" + keyName);
-//   });
-// });
+const canonicalizedAmzHeaders = (request) => {
 
-const getAllVendors = () => {
-  try {
-    return new Promise((resolve, reject) => {
-      s3.getObject(bucketParams, function(err, rawData) {
-        if (err) {
-          throw err;
+  var amzHeaders = [];
+
+  AWS.util.each(request.headers, function (name) {
+    if (name.match(/^x-amz-/i))
+      amzHeaders.push(name);
+  });
+
+  amzHeaders.sort(function (a, b) {
+    return a.toLowerCase() < b.toLowerCase() ? -1 : 1;
+  });
+
+  var parts = [];
+  AWS.util.arrayEach.call(this, amzHeaders, function (name) {
+    parts.push(name.toLowerCase() + ':' + String(request.headers[name]));
+  });
+
+  return parts.join('\n');
+
+};
+
+const canonicalizedResource = (request) => {
+
+  var r = request;
+
+  var parts = r.path.split('?');
+  var path = parts[0];
+  var querystring = parts[1];
+
+  var resource = '';
+
+  if (r.virtualHostedBucket)
+    resource += '/' + r.virtualHostedBucket;
+
+  resource += path;
+
+  if (querystring) {
+
+    // collect a list of sub resources and query params that need to be signed
+    var resources = [];
+
+    AWS.util.arrayEach.call(this, querystring.split('&'), function (param) {
+      var name = param.split('=')[0];
+      var value = param.split('=')[1];
+      if (this.subResources[name] || this.responseHeaders[name]) {
+        var subresource = { name: name };
+        if (value !== undefined) {
+          if (this.subResources[name]) {
+            subresource.value = value;
+          } else {
+            subresource.value = decodeURIComponent(value);
+          }
+        }
+        resources.push(subresource);
+      }
+    });
+
+    resources.sort(function (a, b) { return a.name < b.name ? -1 : 1; });
+
+    if (resources.length) {
+
+      querystring = [];
+      AWS.util.arrayEach(resources, function (res) {
+        if (res.value === undefined) {
+          querystring.push(res.name);
         } else {
-          const data = JSON.parse(rawData.Body.toString('ascii'));
-          resolve(data);
+          querystring.push(res.name + '=' + res.value);
         }
       });
-    })
-  } catch(err) {
-    return err;
-  }
-};
 
-const updateAllVendors = (vendors) => {
-  const bufferData = new Buffer(JSON.stringify(vendors), 'binary');
-  const putParams = {
-    ...bucketParams,
-    Body: bufferData,
-  };
-  try {
-    return new Promise((resolve, reject) => {
-      s3.putObject(putParams, function(err, data) {
-        if (err) throw err;
-        else resolve(data);
-      });
-    })
-  } catch(err) {
-    return err;
+      resource += '?' + querystring.join('&');
+    }
+
   }
+
+  return resource;
 };
 
 router.get('/', async (req, res, next) => {
+  // req is IncomingMessage, we need an HttpRequest instance
   try {
-    const kSecret = 'PQlHg1M+OnKCDnaYH9W+WTkVBlhxccpZgHdcTzbi';
+    const credentials = {
+      accessKeyId: 'AKIAIY6EW7RYBZLFFJGA',
+      secretAccessKey: 'cIwtdr45YiP3obKuY3EHyPlwMwzQAYB/lhtSW0Qt',
+      region: 'ap-southeast-1',
+    };
+    const dateTime = (moment().toISOString().split('.')[0] + 'Z').replace(/[-:]/g,'');
     const date = moment().format('YYYYMMDD');
-    const region = 'ap-southeast-1';
-    const signingKey = hmacSHA256(hmacSHA256(hmacSHA256(hmacSHA256("AWS4" + kSecret, date), region), 's3'),"aws4_request");
-    const signatureKey = hexEncode(hmacSHA256(signingKey, 'getAirportCodes'));
-    console.log('signatureKey: ', signatureKey);
-    // res.send({ signingKey: sha256('') });
-    return fetch('https://s3-ap-southeast-1.amazonaws.com/awss3rnd/airportCodes.csv', {
+    const path = './vendors.json';
+    const request = {
       headers: {
-        'X-Amz-Date': (moment().toISOString().split('.')[0] + 'Z').replace(/[-:]/g,''),
-        'X-Amz-Content-Sha256': sha256(''),
-        'Authorization': 'AWS4-HMAC-SHA256 Credential=AKIAIN56YT42LHRJSFDA/20180418/ap-southeast-1/s3/aws4_request, SignedHeaders=content-type;host;x-amz-content-sha256;x-amz-date, Signature=' + signatureKey,
-        'Content-Type': 'application/xml',
+        'content-type': 'application/x-www-form-urlencoded',
+        'host': 'awss3rnd.s3.amazonaws.com',
+        'x-amz-content-sha256': 'e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855',
+        'x-amz-date': dateTime,
+      },
+      region: 'ap-southeast-1',
+      path,
+      pathname: () => {
+        return path;
+      },
+      search: () => {
+        return '';
       },
       method: 'GET',
-    }).then(response => res.send({ response }))
-      .catch(error => res.send({ error }))
+    };
+
+    const v4 = new AWS.Signers.V4(request, 's3');
+    v4.addAuthorization(credentials, new Date());
+    
+    const s3 = new AWS.Signers.S3();
+    s3.request = request;
+    s3.addAuthorization(credentials, new Date());
+    // const signatureV4 = v4.signature(credentials, dateTime);
+    // const credential = `${credentials.accessKeyId}/${date}/ap-southeast-1/s3/aws4_request`;
+    // const signedHeaders = 'content-type;host;x-amz-content-sha256;x-amz-date';
+    // const content = 'e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855';
+    // const auth = `AWS4-HMAC-SHA256 Credential=${credential}, SignedHeaders=${signedHeaders}, Signature=${signatureV4}`;
+    const headers = {
+      'Content-Type': 'application/x-www-form-urlencoded',
+      'Authorization': v4.authorization(credentials, dateTime),
+      'X-Amz-Content-Sha256': 'e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855',
+      'X-Amz-Date': dateTime,
+    };
+    return fetch('https://s3-ap-southeast-1.amazonaws.com/awss3rnd/airportCodes.csv', {
+      headers,
+      method: 'GET',
+    }).then(response => {
+      console.log('response: ', response);
+      return response.text();
+    }).then(json => res.send({ data: json }))
+      .catch(error => {
+        console.log('error: ', error);
+        return res.send({error});
+      })
     // res.send({ signingKey: base64.stringify(signingKey) });
   } catch(error) {
     next(error);
